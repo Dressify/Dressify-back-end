31345e6f1f352aa1547a3ff0ec7d32e4a052bf5e
eslammMo <eslammohmed2001@gmail.com>
2023-04-08 17:49:28 +0200

    add user and identity tables to DB

---
+++ dressify/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace dressify.Models
+{
+    public class ApplicationUser: IdentityUser
+    {
+
+        public string? ProfilePic { get; set; }
+        public string? Gender { get; set; }
+        public string? Address { get; set; }
+        public DateTime? DOB { get; set; }
+        public int? Age { get; set; }
+    }
+}
