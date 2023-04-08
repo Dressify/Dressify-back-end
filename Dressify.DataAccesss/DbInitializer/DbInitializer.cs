abc7ec08169611e9d0e56f9369b9407e8d376efe
eslammMo <eslammohmed2001@gmail.com>
2023-04-09 00:23:09 +0200

    apply pending Migrations

---
+++ Dressify.DataAccesss/DbInitializer/DbInitializer.cs
@@ -0,0 +1,11 @@
+            //migrations if they are not applied
+            try
+            {
+                if (_context.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _context.Database.Migrate();
+                }
+            }
+            catch (Exception ex) 
+            { 
+            }
