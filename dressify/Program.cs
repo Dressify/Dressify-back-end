ba22c6692003fa600a2c0d7d3107b9e16f88074d
eslammMo <eslammohmed2001@gmail.com>
2023-04-11 21:00:28 +0200

    update project packages

--- dressify/Program.cs
+++ dressify/Program.cs
@@ -11,9 +11,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddScoped<IDbInitializer, DbInitializer>();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(connectionString));
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
