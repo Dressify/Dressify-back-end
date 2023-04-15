7d7f1879e5fff6ebd6061a835177c37b46f175fd
eslammMo <eslammohmed2001@gmail.com>
2023-04-15 03:36:00 +0200

    implement AuthController
    AuthController  have : Register and Login Methods using JWT Token

---
+++ dressify/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using Dressify.DataAccess.Dtos;
+using Dressify.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dressify.Controllers
+{
+    [Route("api/[controller]")]
+
+    public class AuthController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public AuthController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterAsync([FromBody] RegisterDto model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _unitOfWork.ApplicationUser.RegisterAsync(model);
+            return Ok(result);
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> GetTokenAsync([FromBody] TokenRequestDto model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _unitOfWork.ApplicationUser.GetTokenAsync(model);
+
+            if (!result.IsAuthenticated)
+                return BadRequest(result.Message);
+            return Ok(result);
+        }
+
+       
+    }
+}
--- dressify/Program.cs
+++ dressify/Program.cs
@@ -1,10 +1,15 @@
 using Dressify.DataAccess;
 using Dressify.DataAccess.DbInitializer;
+using Dressify.DataAccess.Helpers;
 using Dressify.DataAccess.Repository;
 using Dressify.DataAccess.Repository.IRepository;
 using Dressify.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Configuration;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +19,28 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(connectionString));
 builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+builder.Services.Configure<JWT>(builder.Configuration.GetSection("JWT"));
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(o =>
+    {
+        o.RequireHttpsMetadata = false;
+        o.SaveToken = false;
+        o.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidIssuer = builder.Configuration["JWT:Issuer"],
+            ValidAudience = builder.Configuration["JWT:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"])),
+            ClockSkew = TimeSpan.Zero
+        };
+    });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -33,6 +60,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseCors(c => c.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 SeedDatabase();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
