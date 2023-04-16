1e58e002b8b17a712ff312e26481cc79dbffdb9a
eslammMo <eslammohmed2001@gmail.com>
2023-04-16 17:07:32 +0200

    Add Rate Functionality To Customer

--- dressify/Controllers/CustomerController.cs
+++ dressify/Controllers/CustomerController.cs
@@ -25,7 +25,7 @@ namespace dressify.Controllers
         {
             var user = await _unitOfWork.ApplicationUser.GetUserAsync(obj.CustomerId);
             var product = _unitOfWork.Product.GetById(obj.ProductId);
-            if (user==null)
+            if (user == null)
             {
                 return BadRequest("user does not exist");
             }
@@ -40,6 +40,50 @@ namespace dressify.Controllers
             };
             await _unitOfWork.WishList.AddAsync(item);
             _unitOfWork.Save();
+            return Ok(obj);
+        }
+
+        [HttpPost("CustomerRate")]
+        public async Task<IActionResult> CustomerRateProduct(RateDto obj)
+        {
+            var user = await _unitOfWork.ApplicationUser.GetUserAsync(obj.CustomerId);
+            var product = _unitOfWork.Product.GetById(obj.ProductId);
+            if (user == null)
+            {
+                return BadRequest("user does not exist");
+            }
+            if (product == null)
+            {
+                return BadRequest("product does not exist");
+            }
+            if(obj.rate <1 && obj.rate > 5) 
+            {
+                return BadRequest("Rating should be between 1 to 5 ");
+            }
+            var result = _unitOfWork.ProductRate.Find(r => r.ProductId==obj.ProductId && r.CustomerId==obj.CustomerId);
+
+            if (result is not null )
+            {
+                return BadRequest("This product has already been rated by the customer ");
+            }
+            else
+            {
+                var item = new ProductRate
+                {
+                    CustomerId = obj.CustomerId,
+                    ProductId = obj.ProductId,
+                    rate =obj.rate,
+                    RateComment = obj.RateComment,
+                    //include isPurchased after adding Order 
+                    Date=DateTime.Now,
+                };
+                await _unitOfWork.ProductRate.AddAsync(item);
+                _unitOfWork.Save();
+
+            }
+
+
+            
             return Ok(obj);
         }
     }
