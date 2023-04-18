20948761aa852ce9b7ac6004ada413ba86ff6fc1
eslammMo <eslammohmed2001@gmail.com>
2023-04-18 03:01:23 +0200

    update Vendor Controller

--- dressify/Controllers/VendorController.cs
+++ dressify/Controllers/VendorController.cs
@@ -61,6 +61,44 @@ namespace dressify.Controllers
             _unitOfWork.Save();
             return Ok(question);
         }
+        [HttpPost("AddProduct")]
+        public async Task<IActionResult> AddProduct([FromForm] CreateProductDto dto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("product Data Not Valid");
+            var product = new Product
+            {
+                VendorId = dto.VendorId,
+                ProductName = dto.ProductName,
+                Description = dto.Description,
+                Price = dto.Price,
+                Quantity = dto.Quantity,
+                Sale = dto.Sale,
+                Rentable = dto.Rentable,
+                Color = dto.Color,
+                Category = dto.Category,
+                SubCategory = dto.SubCategory,
+                Type = dto.Type,
+            };
+            _unitOfWork.Product.AddAsync(product);
+            _unitOfWork.Save();
+            foreach (var img in dto.Photos)
+            {
+                CreatePhotoDto result = await _unitOfWork.ProductImage.AddPhoto(img);
+                var extension = System.IO.Path.GetExtension(img.FileName);
+
+                var productImg = new ProductImage
+                {
+                    ProductId = product.ProductId,
+                    PublicId = result.PublicId,
+                    ImageUrl = result.Url,
+                    ImageExtension = extension,
+                };
+                _unitOfWork.ProductImage.Add(productImg);
+                _unitOfWork.Save();
+            }
+            return Ok();
+        }
 
     }
 }
