34ab4d80a3533d9a64352343116aef0a64c5b58e
eslammMo <eslammohmed2001@gmail.com>
2023-04-18 21:55:03 +0200

    add ProductDetails API

---
+++ dressify/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Dressify.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dressify.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet("listAllProducts")] 
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var products = await _unitOfWork.Product.GetAllAsync(new[] { "Vendor", "ProductImages", "Questions" });
+            return Ok(products);
+        }
+        [HttpGet("GetProductDetails")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            
+            var product = await _unitOfWork.Product.FindAsync(p=> p.ProductId == id , new[] { "Vendor", "ProductImages", "Questions" }
+);              if(product == null)
+                    return NotFound();
+            return Ok(product);
+        }
+    }
+}
+
