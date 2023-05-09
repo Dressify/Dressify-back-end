aeefa268407245932da7dc4293caf9e9e553451e
eslammMo <eslammohmed2001@gmail.com>
2023-05-09 22:00:52 +0300

    add vendor view and edit profile APIs

---
+++ Dressify.DataAccesss/Dtos/VendorProfileDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dressify.DataAccess.Dtos
+{
+    public class VendorProfileDto
+    {
+        public string? Address { get; set; }
+        public string? FName { get; set; }
+        public string? LName { get; set; }
+        public string? Email { get; set; }
+        public string? UserName { get; set; }
+        public string? imgUrl { get; set; }
+        public string? PhoneNumber { get; set;}
+    }
+}
