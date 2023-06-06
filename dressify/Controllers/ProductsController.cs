f8aac81a055b54b797b262d248b962e269adbdf0
eslammMo <eslammohmed2001@gmail.com>
2023-06-06 06:26:35 +0300

    Update ProductsController.cs

--- dressify/Controllers/ProductsController.cs
+++ dressify/Controllers/ProductsController.cs
@@ -160,9 +160,9 @@ namespace dressify.Controllers
                 return NoContent();
             }
     
-            var categories = distinctValues.Where(p => p.Category != null).Select(p => p.Category).Distinct().ToList();
-            var subCategories = distinctValues.Where(p => p.SubCategory != null).Select(p => p.SubCategory).Distinct().ToList();
-            var types = distinctValues.Where(p => p.Type != null).Select(p => p.Type).Distinct().ToList();
+            var categories = distinctValues.Where(p => p.Category != null).Where(p => p.Category != "").Select(p => p.Category).Distinct().ToList();
+            var subCategories = distinctValues.Where(p => p.SubCategory != null).Where(p => p.SubCategory != "").Select(p => p.SubCategory).Distinct().ToList();
+            var types = distinctValues.Where(p => p.Type != null).Where(p => p.Type != "").Select(p => p.Type).Distinct().ToList();
 
             return Ok(new { categories, subCategories, types });
         }
